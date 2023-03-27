7da547a1676167b1509e86f815d4ca0e9707e384
HONGJIBEOM <corea1836@naver.com>
2023-03-27 18:18:07 +0900

    test controller

---
+++ src/main/java/flab/testController/Sample.java
@@ -0,0 +1,35 @@
+package flab.testController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@RestController
+public class Sample {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/helloDB")
+    public String helloDB() {
+        String result = null;
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://27.96.135.218:3306/test",
+                    "readonly",
+                    "0000"
+            );
+            if(con != null) {
+                result = con.toString();
+            }
+        } catch (SQLException e) {
+            result = e.toString();
+        }
+        return result;
+    }
+}
