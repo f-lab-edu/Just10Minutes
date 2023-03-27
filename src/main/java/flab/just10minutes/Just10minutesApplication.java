d2ffe4a69c12f6c1cc989cfdfcc0a767dd42070f
HONGJIBEOM <corea1836@naver.com>
2023-03-27 16:52:09 +0900

    first commit

---
+++ src/main/java/flab/just10minutes/Just10minutesApplication.java
@@ -0,0 +1,13 @@
+package flab.just10minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Just10minutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Just10minutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/just10minutes/Just10minutesApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.just10minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Just10minutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
